0c8ded9e82e60a615959324e0a7be80b4dfd9fce
maguilo11 <aguilo.miguel@gmail.com>
2020-03-14 16:27:41 -0600

    implemented surface pressure

--- src/NaturalBCs.hpp
+++ src/NaturalBCs.hpp
@@ -353,7 +353,145 @@ ComputeSurfaceIntegralWeight<3>::operator()
 }
 
 /***************************************************************************//**
- * \brief Class for natural boundary condition surface integrals.
+ * \brief Class for the evaluation of natural boundary condition surface integrals
+ * of type: UNIFORM PRESSURE.
+ *
+ * \tparam SpatialDim   spatial dimension
+ * \tparam NumDofs      number degrees of freedom per natural boundary condition force vector
+ * \tparam DofsPerNode  number degrees of freedom per node
+ * \tparam DofOffset    degrees of freedom offset
+ *
+*******************************************************************************/
+template<Plato::OrdinalType SpatialDim, Plato::OrdinalType NumDofs=SpatialDim, Plato::OrdinalType DofsPerNode=NumDofs, Plato::OrdinalType DofOffset=0>
+class SurfacePressureIntegral
+{
+private:
+    const std::string mSideSetName;
+    const Omega_h::Vector<NumDofs> mFlux;
+    Plato::LinearTetCubRuleDegreeOne<SpatialDim> mCubatureRule;
+
+public:
+    /******************************************************************************//**
+     * \brief Constructor
+     **********************************************************************************/
+    SurfacePressureIntegral(const std::string & aSideSetName, const Omega_h::Vector<NumDofs>& aFlux);
+
+    /***************************************************************************//**
+     * \brief Evaluate natural boundary condition surface integrals.
+     *
+     * \tparam StateScalarType   state forward automatically differentiated (FAD) type
+     * \tparam ControlScalarType control FAD type
+     * \tparam ConfigScalarType  configuration FAD type
+     * \tparam ResultScalarType  result FAD type
+     *
+     * \param [in]  aMesh     Omega_h mesh database.
+     * \param [in]  aMeshSets Omega_h side set database.
+     * \param [in]  aState    2-D view of state variables.
+     * \param [in]  aControl  2-D view of control variables.
+     * \param [in]  aConfig   3-D view of configuration variables.
+     * \param [out] aResult   Assembled vector to which the boundary terms will be added
+     * \param [in]  aScale    scalar multiplier
+     *
+    *******************************************************************************/
+    template<typename StateScalarType,
+             typename ControlScalarType,
+             typename ConfigScalarType,
+             typename ResultScalarType>
+    void operator()(Omega_h::Mesh* aMesh,
+                    const Omega_h::MeshSets &aMeshSets,
+                    const Plato::ScalarMultiVectorT<  StateScalarType>& aState,
+                    const Plato::ScalarMultiVectorT<ControlScalarType>& aControl,
+                    const Plato::ScalarArray3DT    < ConfigScalarType>& aConfig,
+                    const Plato::ScalarMultiVectorT< ResultScalarType>& aResult,
+                    Plato::Scalar aScale) const;
+};
+
+/***************************************************************************//**
+ * \brief SurfacePressureIntegral::SurfacePressureIntegral constructor definition
+*******************************************************************************/
+template<Plato::OrdinalType SpatialDim, Plato::OrdinalType NumDofs, Plato::OrdinalType DofsPerNode, Plato::OrdinalType DofOffset>
+SurfacePressureIntegral<SpatialDim,NumDofs,DofsPerNode,DofOffset>::SurfacePressureIntegral
+(const std::string & aSideSetName, const Omega_h::Vector<NumDofs>& aFlux) :
+    mSideSetName(aSideSetName),
+    mFlux(aFlux)
+{}
+
+/***************************************************************************//**
+ * \brief SurfacePressureIntegral::operator() function definition
+*******************************************************************************/
+template<Plato::OrdinalType SpatialDim, Plato::OrdinalType NumDofs, Plato::OrdinalType DofsPerNode, Plato::OrdinalType DofOffset>
+template<typename StateScalarType,
+         typename ControlScalarType,
+         typename ConfigScalarType,
+         typename ResultScalarType>
+void SurfacePressureIntegral<SpatialDim,NumDofs,DofsPerNode,DofOffset>::operator()
+(Omega_h::Mesh* aMesh,
+ const Omega_h::MeshSets &aMeshSets,
+ const Plato::ScalarMultiVectorT<  StateScalarType>& aState,
+ const Plato::ScalarMultiVectorT<ControlScalarType>& aControl,
+ const Plato::ScalarArray3DT    < ConfigScalarType>& aConfig,
+ const Plato::ScalarMultiVectorT< ResultScalarType>& aResult,
+ Plato::Scalar aScale) const
+{
+    // get sideset faces
+    auto tFaceLids = Plato::get_face_local_ordinals(aMeshSets, mSideSetName);
+    auto tNumFaces = tFaceLids.size();
+
+    // get mesh vertices
+    auto tCoords = aMesh->coords();
+    auto tFace2Verts = aMesh->ask_verts_of(SpatialDim-1);
+    auto tCell2Verts = aMesh->ask_elem_verts();
+
+    auto tFace2eElems = aMesh->ask_up(SpatialDim - 1, SpatialDim);
+    auto tFace2Elems_map   = tFace2eElems.a2ab;
+    auto tFace2Elems_elems = tFace2eElems.ab2b;
+
+    Plato::ComputeSurfaceJacobians<SpatialDim> tComputeSurfaceJacobians;
+    Plato::ComputeSurfaceIntegralWeight<SpatialDim> tComputeSurfaceIntegralWeight;
+    Plato::CreateFaceLocalNode2ElemLocalNodeIndexMap<SpatialDim> tCreateFaceLocalNode2ElemLocalNodeIndexMap;
+    Plato::ScalarArray3DT<ConfigScalarType> tJacobian("jacobian", tNumFaces, SpatialDim-1, SpatialDim);
+
+    auto tFlux = mFlux;
+    auto tNodesPerFace = SpatialDim;
+    auto tCubatureWeight = mCubatureRule.getCubWeight();
+    Kokkos::parallel_for(Kokkos::RangePolicy<>(0,tNumFaces), LAMBDA_EXPRESSION(const Plato::OrdinalType & aFaceIndex)
+    {
+
+      auto tFaceOrdinal = tFaceLids[aFaceIndex];
+
+      // for each element that the face is connected to: (either 1 or 2)
+      for( Plato::OrdinalType tLocalElemOrd = tFace2Elems_map[tFaceOrdinal]; tLocalElemOrd < tFace2Elems_map[tFaceOrdinal+1]; ++tLocalElemOrd )
+      {
+          // create a map from face local node index to elem local node index
+          Plato::OrdinalType tLocalNodeOrd[SpatialDim];
+          auto tCellOrdinal = tFace2Elems_elems[tLocalElemOrd];
+          tCreateFaceLocalNode2ElemLocalNodeIndexMap(tCellOrdinal, aFaceIndex, tCell2Verts, tFace2Verts, tLocalNodeOrd);
+
+          ConfigScalarType tWeight(0.0);
+          auto tMultiplier = aScale / tCubatureWeight;
+          tComputeSurfaceJacobians(tCellOrdinal, aFaceIndex, tLocalNodeOrd, aConfig, tJacobian);
+          tComputeSurfaceIntegralWeight(aFaceIndex, tMultiplier, tJacobian, tWeight);
+
+          // compute unit normal vector
+          auto tCellPoints = Plato::local_element_coords(tCellOrdinal, tCoords, tCell2Verts);
+          auto tUnitNormalVec = Plato::unit_normal_vector(tFaceOrdinal, tCellPoints);
+
+          // project into aResult workset
+          for( Plato::OrdinalType tNode=0; tNode<tNodesPerFace; tNode++)
+          {
+              for( Plato::OrdinalType tDof=0; tDof<NumDofs; tDof++)
+              {
+                  auto tCellDofOrdinal = tLocalNodeOrd[tNode] * DofsPerNode + tDof + DofOffset;
+                  aResult(tCellOrdinal,tCellDofOrdinal) += tWeight*tUnitNormalVec[tDof]*tFlux[tDof];
+              }
+          }
+      }
+    }, "surface pressure integral");
+}
+
+/***************************************************************************//**
+ * \brief Class for the evaluation of natural boundary condition surface integrals
+ * of type: UNIFORM and UNIFORM COMPONENT.
  *
  * \tparam SpatialDim   spatial dimension
  * \tparam NumDofs      number degrees of freedom per natural boundary condition force vector
@@ -405,6 +543,9 @@ public:
                     Plato::Scalar aScale) const;
 };
 
+/***************************************************************************//**
+ * \brief SurfaceLoadIntegral::SurfaceLoadIntegral constructor definition
+*******************************************************************************/
 template<Plato::OrdinalType SpatialDim, Plato::OrdinalType NumDofs, Plato::OrdinalType DofsPerNode, Plato::OrdinalType DofOffset>
 SurfaceLoadIntegral<SpatialDim,NumDofs,DofsPerNode,DofOffset>::SurfaceLoadIntegral
 (const std::string & aSideSetName, const Omega_h::Vector<NumDofs>& aFlux) :
@@ -412,6 +553,9 @@ SurfaceLoadIntegral<SpatialDim,NumDofs,DofsPerNode,DofOffset>::SurfaceLoadIntegr
     mFlux(aFlux)
 {}
 
+/***************************************************************************//**
+ * \brief SurfaceLoadIntegral::operator() function definition
+*******************************************************************************/
 template<Plato::OrdinalType SpatialDim, Plato::OrdinalType NumDofs, Plato::OrdinalType DofsPerNode, Plato::OrdinalType DofOffset>
 template<typename StateScalarType,
          typename ControlScalarType,
@@ -718,6 +862,8 @@ void NaturalBC<SpatialDim,NumDofs,DofsPerNode,DofOffset>::get
         }
         case Plato::Neumann::UNIFORM_PRESSURE:
         {
+            Plato::SurfacePressureIntegral<SpatialDim, NumDofs, DofsPerNode, DofOffset> tSurfacePress(mSideSetName, mFlux);
+            tSurfacePress(aMesh, aMeshSets, aState, aControl, aConfig, aResult, aScale);
             break;
         }
         default:
--- src/OmegaHUtilities.hpp
+++ src/OmegaHUtilities.hpp
@@ -14,6 +14,105 @@ namespace Plato
 {
 
 /***************************************************************************//**
+ * \brief 1-D specialization: Return local element/cell coordinates, i.e.
+ * coordinates for each node.
+ *
+ * \param [in] aCellOrdinal cell ordinal
+ * \param [in] aCoords      mesh coordinates
+ * \param [in] aCell2Verts  cell to local vertex id map
+ *
+ * \return node coordinates for element with ordinal: aCellOrdinal
+ *
+*******************************************************************************/
+inline Omega_h::Few< Omega_h::Vector<1>, 2> local_element_coords
+(const Plato::OrdinalType & aCellOrdinal,
+ const Omega_h::Reals & aCoords,
+ const Omega_h::LOs & aCell2Verts)
+{
+    const Plato::OrdinalType tSpaceDim = 1;
+    const Plato::OrdinalType tNodesPerCell = 2;
+    Omega_h::Few<Omega_h::Vector<tSpaceDim>, tNodesPerCell> tCellPoints;
+
+    for (Plato::OrdinalType jNode = 0; jNode < tNodesPerCell; jNode++)
+    {
+        const auto tVertexLocalID = aCell2Verts[aCellOrdinal * tNodesPerCell + jNode];
+        for (Plato::OrdinalType tDim = 0; tDim < tSpaceDim; tDim++)
+        {
+            tCellPoints[jNode][tDim] = aCoords[tVertexLocalID * tSpaceDim + tDim];
+        }
+    }
+
+    return tCellPoints;
+}
+// local_element_coords : 1-D
+
+/***************************************************************************//**
+ * \brief 2-D specialization: Return local element/cell coordinates, i.e.
+ * coordinates for each node.
+ *
+ * \param [in] aCellOrdinal cell ordinal
+ * \param [in] aCoords      mesh coordinates
+ * \param [in] aCell2Verts  cell to local vertex id map
+ *
+ * \return node coordinates for element with ordinal: aCellOrdinal
+ *
+*******************************************************************************/
+inline Omega_h::Few< Omega_h::Vector<2>, 3> local_element_coords
+(const Plato::OrdinalType & aCellOrdinal,
+ const Omega_h::Reals & aCoords,
+ const Omega_h::LOs & aCell2Verts)
+{
+    const Plato::OrdinalType tSpaceDim = 2;
+    const Plato::OrdinalType tNodesPerCell = 3;
+    Omega_h::Few<Omega_h::Vector<tSpaceDim>, tNodesPerCell> tCellPoints;
+
+    for (Plato::OrdinalType jNode = 0; jNode < tNodesPerCell; jNode++)
+    {
+        const auto tVertexLocalID = aCell2Verts[aCellOrdinal * tNodesPerCell + jNode];
+        for (Plato::OrdinalType tDim = 0; tDim < tSpaceDim; tDim++)
+        {
+            tCellPoints[jNode][tDim] = aCoords[tVertexLocalID * tSpaceDim + tDim];
+        }
+    }
+
+    return tCellPoints;
+}
+// local_element_coords : 2-D
+
+/***************************************************************************//**
+ * \brief 3-D specialization: Return local element/cell coordinates, i.e.
+ * coordinates for each node.
+ *
+ * \param [in] aCellOrdinal cell ordinal
+ * \param [in] aCoords      mesh coordinates
+ * \param [in] aCell2Verts  cell to local vertex id map
+ *
+ * \return node coordinates for element with ordinal: aCellOrdinal
+ *
+*******************************************************************************/
+inline Omega_h::Few< Omega_h::Vector<3>, 4> local_element_coords
+(const Plato::OrdinalType & aCellOrdinal,
+ const Omega_h::Reals & aCoords,
+ const Omega_h::LOs & aCell2Verts)
+{
+    const Plato::OrdinalType tSpaceDim = 3;
+    const Plato::OrdinalType tNodesPerCell = 4;
+    Omega_h::Few<Omega_h::Vector<tSpaceDim>, tNodesPerCell> tCellPoints;
+
+    for (Plato::OrdinalType jNode = 0; jNode < tNodesPerCell; jNode++)
+    {
+        const auto tVertexLocalID = aCell2Verts[aCellOrdinal * tNodesPerCell + jNode];
+        for (Plato::OrdinalType tDim = 0; tDim < tSpaceDim; tDim++)
+        {
+            tCellPoints[jNode][tDim] = aCoords[tVertexLocalID * tSpaceDim + tDim];
+        }
+    }
+
+    return tCellPoints;
+}
+// local_element_coords : 3-D
+
+/***************************************************************************//**
  * \brief Return face local ordinals for each element on the requested side set
  *
  * \param [in] aMeshSets    Omega_h side set database
@@ -39,7 +138,18 @@ inline Omega_h::LOs get_face_local_ordinals(const Omega_h::MeshSets& aMeshSets, 
 // function get_face_local_ordinals
 
 /******************************************************************************//**
-* \brief Return normalized vector : 2-D specialization
+* \brief Normalized vector : 1-D specialization
+*
+* \param [in/out] aVector  Omega_H vector
+*
+* \return normalized vector
+*
+**********************************************************************************/
+inline void normalize(Omega_h::Vector<1> & aVector) { return; }
+// function normalize - 1D
+
+/******************************************************************************//**
+* \brief Normalized vector : 2-D specialization
 *
 * \param [in/out] aVector  Omega_H vector
 *
@@ -55,7 +165,7 @@ inline void normalize(Omega_h::Vector<2> & aVector)
 // function normalize - 2D
 
 /******************************************************************************//**
-* \brief Return normalized vector : 3-D specialization
+* \brief Normalized vector : 3-D specialization
 *
 * \param [in/out] aVector  Omega_H vector
 *
@@ -70,6 +180,57 @@ inline void normalize(Omega_h::Vector<3> & aVector)
     aVector[2] = aVector[2] / tMagnitude;
 }
 // function normalize - 3D
+
+/******************************************************************************//**
+* \brief Return unit normal vector : 1-D specialization
+*
+* \param [in/out] aVector  Omega_H vector
+*
+* \return unit normal vector
+*
+**********************************************************************************/
+inline Omega_h::Vector<1> unit_normal_vector
+(const Omega_h::LO & aFaceOrdinal, const Omega_h::Few< Omega_h::Vector<1>, 2> & aCellPoints)
+{
+    auto tNormalVec = Omega_h::get_side_vector(aCellPoints, aFaceOrdinal);
+    Plato::normalize(tNormalVec);
+    return tNormalVec;
+}
+// function unit_normal_vector - 1D
+
+/******************************************************************************//**
+* \brief Return unit normal vector : 2-D specialization
+*
+* \param [in/out] aVector  Omega_H vector
+*
+* \return unit normal vector
+*
+**********************************************************************************/
+inline Omega_h::Vector<2> unit_normal_vector
+(const Omega_h::LO & aFaceOrdinal, const Omega_h::Few< Omega_h::Vector<2>, 3> & aCellPoints)
+{
+    auto tNormalVec = Omega_h::get_side_vector(aCellPoints, aFaceOrdinal);
+    Plato::normalize(tNormalVec);
+    return tNormalVec;
+}
+// function unit_normal_vector - 2D
+
+/******************************************************************************//**
+* \brief Return unit normal vector : 3-D specialization
+*
+* \param [in/out] aVector  Omega_H vector
+*
+* \return unit normal vector
+*
+**********************************************************************************/
+inline Omega_h::Vector<3> unit_normal_vector
+(const Omega_h::LO & aFaceOrdinal, const Omega_h::Few< Omega_h::Vector<3>, 4> & aCellPoints)
+{
+    auto tNormalVec = Omega_h::get_side_vector(aCellPoints, aFaceOrdinal);
+    Plato::normalize(tNormalVec);
+    return tNormalVec;
+}
+// function unit_normal_vector - 3D
 
 /******************************************************************************//**
 * \brief Create an Omega_H write array
