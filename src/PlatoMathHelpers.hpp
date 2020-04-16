4153b6a30c910ab92954497fa9af484ca1010669
maguilo11 <aguilo.miguel@gmail.com>
2020-04-16 14:49:12 -0600

    improved error messages

--- src/PlatoMathHelpers.hpp
+++ src/PlatoMathHelpers.hpp
@@ -70,8 +70,8 @@ inline void random(const Plato::Scalar & aLowerBound, const Plato::Scalar & aUpp
 
 /******************************************************************************//**
  * \brief Compute inner product
- * \param [in] aVec1 1D container
- * \param [in] aVec2 1D container
+ * \param [in] aVec1 1D Kokkos container
+ * \param [in] aVec2 1D Kokkos container
  * \return inner product
 **********************************************************************************/
 template<typename VecOneT, typename VecTwoT>
@@ -80,8 +80,9 @@ inline Plato::Scalar dot(const VecOneT & aVec1, const VecTwoT & aVec2)
     if(aVec2.size() != aVec1.size())
     {
         std::stringstream tMsg;
-        tMsg << "DIMENSION MISMATCH. VECTOR ONE HAS SIZE = " << aVec1.size()
-                << " AND VECTOR TWO HAS SIZE = " << aVec2.size() << ".\n";
+        tMsg << "BLAS1 DOT: DIMENSION MISMATCH. VECTOR ONE HAS SIZE = " << aVec1.size() << " AND VECTOR TWO HAS SIZE = "
+            << aVec2.size() << ".VECTOR ONE HAS LABEL '" << aVec1.label() << "' AND VECTOR TWO HAS LABEL '"
+            << aVec2.label() << "'.\n";
         THROWERR(tMsg.str().c_str())
     }
 
@@ -98,7 +99,7 @@ inline Plato::Scalar dot(const VecOneT & aVec1, const VecTwoT & aVec2)
 
 /******************************************************************************//**
  * \brief Compute the norm/length of a vector
- * \param [in] aVector 1D container
+ * \param [in] aVector 1D Kokkos container
  * \return norm/length
 **********************************************************************************/
 template<typename VecOneT>
@@ -106,26 +107,34 @@ inline Plato::Scalar norm(const VecOneT & aVector)
 {
     if(aVector.size() <= static_cast<Plato::OrdinalType>(0))
     {
-        THROWERR("INPUT VECTOR IS EMPTY, I.E. SIZE <= 0.")
+        std::stringstream tMsg;
+        tMsg << "BLAS 1 NORM: INPUT VECTOR WITH LABEL '" << aVector.label() << "' IS EMPTY.\n"
+        THROWERR(tMsg.str().c_str())
     }
-
-    const Plato::Scalar tDot = Plato::dot(aVector, aVector);
-    const Plato::Scalar tOutput = std::sqrt(tDot);
+    const auto tDot = Plato::dot(aVector, aVector);
+    const auto tOutput = std::sqrt(tDot);
     return (tOutput);
 }
 // function norm
 
 /******************************************************************************//**
  * \brief Set all the elements to a scalar value
- * \param [in] aInput scalar value
- * \param [out] aVector 1D container
+ * \param [in] aInput   scalar value
+ * \param [out] aVector 1D Kokkos container
 **********************************************************************************/
 template<typename VectorT>
 inline void fill(const Plato::Scalar & aInput, const VectorT & aVector)
 {
     if(aVector.size() <= static_cast<Plato::OrdinalType>(0))
     {
-        THROWERR("INPUT VECTOR IS EMPTY, I.E. SIZE <= 0.")
+        std::stringstream tMsg;
+        tMsg << "BLAS 1 FILL: INPUT VECTOR WITH LABEL '" << aVector.label() << "' IS EMPTY.\n"
+        THROWERR(tMsg.str().c_str())
+    }
+
+    if(std::isfinite(aInput) == false)
+    {
+        THROWERR("BLAS 1 FILL: INPUT SCALAR IS NOT A FINITE NUMBER.\n")
     }
 
     Plato::OrdinalType tNumLocalVals = aVector.size();
@@ -140,8 +149,8 @@ inline void fill(const Plato::Scalar & aInput, const VectorT & aVector)
  * \brief Set all the elements to a scalar value
  * \param [in] aMultiplier scalar multiplier
  * \param [in] aOrdinals   list of entry ordinals
- * \param [in] aValues     list of scalar values
- * \param [out] aVector    1D container
+ * \param [in] aValues     list of scalar values (1D Kokkos container)
+ * \param [out] aVector    1D Kokkos container
 **********************************************************************************/
 template<typename VectorT>
 inline void fill(const Plato::Scalar & aMultiplier,
@@ -151,20 +160,29 @@ inline void fill(const Plato::Scalar & aMultiplier,
 {
     if(aOutput.size() <= static_cast<Plato::OrdinalType>(0))
     {
-        THROWERR("BLAS 1 Fill: INPUT VECTOR IS EMPTY, I.E. SIZE <= 0.")
+        std::stringstream tMsg;
+        tMsg << "BLAS 1 FILL: OUTPUT VECTOR WITH LABEL '" << aOutput.label() << "' IS EMPTY.\n"
+        THROWERR(tMsg.str().c_str())
+    }
+
+    if(aValues.size() <= static_cast<Plato::OrdinalType>(0))
+    {
+        std::stringstream tMsg;
+        tMsg << "BLAS 1 FILL: INPUT VECTOR WITH LABEL '" << aValues.label() << "' IS EMPTY.\n"
+        THROWERR(tMsg.str().c_str())
     }
 
     if(std::isfinite(aMultiplier) == false)
     {
-        THROWERR("BLAS 1 Fill: Detected a non-finite scalar multiplier.")
+        THROWERR("BLAS 1 FILL: SCALAR MULTIPLIER IS NOT A FINITE NUMBER.")
     }
 
     if(aOrdinals.size() != aValues.size())
     {
         std::ostringstream tMsg;
-        tMsg << "BLAS 1 Fill: Dimension mismatch, input list of ordinals and values have different size. "
-                << "List of ordinals has '" << aOrdinals.size() << "' entries and list of values "
-                << "has '" << aValues.size() << "' entries.";
+        tMsg << "BLAS 1 FILL: DIMENSION MISMATCH. INPUT LIST OF ORDINALS AND VALUES HAVE DIFFERENT LENGTH. "
+                << "LIST OF ORDINALS WITH LABEL '" << aOrdinals.label() << "' HAS LENGTH '"<< aOrdinals.size()
+                << "' AND LIST OF VALUES WITH LABEL '" << aValues.label() << "' HAS LENGTH '" << aValues.size() << "'\n.";
         THROWERR(tMsg.str().c_str())
     }
 
@@ -178,8 +196,8 @@ inline void fill(const Plato::Scalar & aMultiplier,
 
 /******************************************************************************//**
  * \brief Copy input 1D container into output 1D container
- * \param [in] aInput 1D container
- * \param [out] aOutput 1D container
+ * \param [in] aInput   1D Kokkos container
+ * \param [out] aOutput 1D Kokkos container
 **********************************************************************************/
 template<typename VecOneT, typename VecTwoT>
 inline void copy(const VecOneT & aInput, const VecTwoT & aOutput)
@@ -187,8 +205,8 @@ inline void copy(const VecOneT & aInput, const VecTwoT & aOutput)
     if(aInput.size() != aOutput.size())
     {
         std::stringstream tMsg;
-        tMsg << "DIMENSION MISMATCH. INPUT VECTOR HAS SIZE = " << aInput.size()
-                << " AND OUTPUT VECTOR HAS SIZE = " << aOutput.size() << ".\n";
+        tMsg << "BLAS 1 COPY: DIMENSION MISMATCH. INPUT VECTOR WITH LABEL '" << aInput.label() << "' HAS LENGTH '"
+        << aInput.size() << "' AND OUTPUT VECTOR WITH LABEL '" << aOutput.label() << "' HAS LENGTH '" << aOutput.size() << "'.";
         THROWERR(tMsg.str().c_str())
     }
 
@@ -202,15 +220,22 @@ inline void copy(const VecOneT & aInput, const VecTwoT & aOutput)
 
 /******************************************************************************//**
  * \brief Scale all the elements by input scalar value
- * \param [in] aInput scalar value
- * \param [out] aOutput 1D container
+ * \param [in] aInput   scalar value
+ * \param [out] aOutput 1D Kokkos container
 **********************************************************************************/
 template<typename VecT>
 inline void scale(const Plato::Scalar & aInput, const VecT & aVector)
 {
+    if(std::isfinite(aInput) == false)
+    {
+        THROWERR("BLAS 1 SCALE: SCALAR MULTIPLIER IS NOT A FINITE NUMBER.")
+    }
+
     if(aVector.size() <= static_cast<Plato::OrdinalType>(0))
     {
-        THROWERR("INPUT VECTOR IS EMPTY, I.E. SIZE <= 0.")
+        std::stringstream tMsg;
+        tMsg << "BLAS 1 SCALE: INPUT VECTOR WITH LABEL '" << aVector.label() << "' IS EMPTY.";
+        THROWERR(tMsg.str().c_str())
     }
 
     Plato::OrdinalType tNumLocalVals = aVector.size();
@@ -223,9 +248,9 @@ inline void scale(const Plato::Scalar & aInput, const VecT & aVector)
 
 /******************************************************************************//**
  * \brief Update elements of B with scaled values of A, /f$ B = B + alpha*A /f$
- * \param [in] aAlpha multiplier of 1D container A
- * \param [in] aInput input 1D container
- * \param [out] aOutput output 1D container
+ * \param [in] aAlpha   multiplier of 1D container A
+ * \param [in] aInput   input 1D Kokkos container
+ * \param [out] aOutput output 1D Kokkos container
 **********************************************************************************/
 template<typename VecT>
 inline void axpy(const Plato::Scalar & aAlpha, const VecT & aInput, const VecT & aOutput)
@@ -249,18 +274,29 @@ inline void axpy(const Plato::Scalar & aAlpha, const VecT & aInput, const VecT &
 /******************************************************************************//**
  * \brief Update elements of B with scaled values of A, /f$ B = beta*B + alpha*A /f$
  * \param [in] aAlpha multiplier of 1D container A
- * \param [in] aInput input 1D container
- * \param [in] aBeta multiplier of 1D container B
- * \param [out] aOutput output 1D container
+ * \param [in] aInput   input 1D Kokkos container
+ * \param [in] aBeta    multiplier of 1D container B
+ * \param [out] aOutput output 1D Kokkos container
 **********************************************************************************/
 template<typename VecT>
 void update(const Plato::Scalar & aAlpha, const VecT & aInput, const Plato::Scalar & aBeta, const VecT & aOutput)
 {
+    if(std::isfinite(aAlpha) == false)
+    {
+        THROWERR("BLAS 1 UPDATE: SCALAR MULTIPLIER 'ALPHA' IS NOT A FINITE NUMBER.")
+    }
+
+    if(std::isfinite(aBeta) == false)
+    {
+        THROWERR("BLAS 1 UPDATE: SCALAR MULTIPLIER 'BETA' IS NOT A FINITE NUMBER.")
+    }
+
     if(aInput.size() != aOutput.size())
     {
         std::stringstream tMsg;
-        tMsg << "DIMENSION MISMATCH. INPUT VECTOR HAS SIZE = " << aInput.size()
-                << " AND OUTPUT VECTOR HAS SIZE = " << aOutput.size() << ".\n";
+        tMsg << "BLAS 1 UPDATE: DIMENSION MISMATCH. INPUT VECTOR WITH LABEL '" << aInput.label() << "' HAS LENGTH '"
+                << aInput.size() << "' AND OUTPUT VECTOR WITH LABEL '" << aOutput.label() << "' HAS LENGTH '"
+                << aOutput.size() << "'.\n";
         THROWERR(tMsg.str().c_str())
     }
 
@@ -274,12 +310,19 @@ void update(const Plato::Scalar & aAlpha, const VecT & aInput, const Plato::Scal
 
 /******************************************************************************//**
  * \brief Reduced operation: sum all the elements in input array and return local sum
- * \param [in] aInput 1D container
+ * \param [in] aInput 1D Kokkos container
  * \param [out] aOutput local sum
 **********************************************************************************/
 template<typename VecT, typename ScalarT>
 void local_sum(const VecT & aInput, ScalarT & aOutput)
 {
+    if(aInput.size() <= static_cast<Plato::OrdinalType>(0))
+    {
+        std::stringstream tMsg;
+        tMsg << "BLAS 1 LOCAL_SUM: INPUT VECTOR WITH LABEL '" << aInput.label() << "' IS EMPTY.";
+        THROWERR(tMsg.str().c_str())
+    }
+
     ScalarT tOutput = 0.0;
     const Plato::OrdinalType tNumLocalElems = aInput.size();
     Kokkos::parallel_reduce(Kokkos::RangePolicy<>(0, tNumLocalElems), LAMBDA_EXPRESSION(const Plato::OrdinalType & aCellOrdinal, ScalarT & aLocalSum)
@@ -389,10 +432,12 @@ void max(const VecT & aInput, ScalarT & aOutput)
 {
     if(aInput.size() <= static_cast<Plato::OrdinalType>(0))
     {
-        THROWERR("INPUT VECTOR IS EMPTY, I.E. SIZE <= 0.")
+        std::ostringstream tMsg;
+        tMsg << "BLAS 1 MAX: INPUT VECTOR WITH LABEL '" << aInput.label() << "' IS EMPTY.";
+        THROWERR(tMsg.str().c_str())
     }
 
-    const OrdinalType tSize = aInput.size();
+    const Plato::OrdinalType tSize = aInput.size();
     //const ScalarT* tInputData = aInput.data();
     aOutput = 0.0;
 
@@ -414,10 +459,12 @@ void min(const VecT & aInput, ScalarT & aOutput)
 {
     if(aInput.size() <= static_cast<Plato::OrdinalType>(0))
     {
-        THROWERR("INPUT VECTOR IS EMPTY, I.E. SIZE <= 0.")
+        std::ostringstream tMsg;
+        tMsg << "BLAS 1 MIN: INPUT VECTOR WITH LABEL '" << aInput.label() << "' IS EMPTY.";
+        THROWERR(tMsg.str().c_str())
     }
 
-    const OrdinalType tSize = aInput.size();
+    const Plato::OrdinalType tSize = aInput.size();
     //const ScalarT* tInputData = aInput.data();
     aOutput = 0.0;
 
@@ -444,6 +491,20 @@ void min(const VecT & aInput, ScalarT & aOutput)
 template<int NumStride, int NumOffset>
 inline void extract(const Plato::ScalarVector& aFromVector, Plato::ScalarVector& aToVector)
 {
+    if(aFromVector.size() <= static_cast<Plato::OrdinalType>(0))
+    {
+        std::ostringstream tMsg;
+        tMsg << "BLAS 1 EXTRACT: FROM VECTOR WITH LABEL '" << aFromVector.label() << "' IS EMPTY.";
+        THROWERR(tMsg.str().c_str())
+    }
+
+    if(aToVector.size() <= static_cast<Plato::OrdinalType>(0))
+    {
+        std::ostringstream tMsg;
+        tMsg << "BLAS 1 EXTRACT: TO VECTOR WITH LABEL '" << aToVector.label() << "' IS EMPTY.";
+        THROWERR(tMsg.str().c_str())
+    }
+
     auto tNumRows = aToVector.extent(0);
 
     Kokkos::parallel_for(Kokkos::RangePolicy<>(0, tNumRows), LAMBDA_EXPRESSION(const Plato::OrdinalType & aOrdinal)
