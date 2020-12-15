dcb23360cdb42a2b52e094e28d82db804bb8aaf5
maguilo11 <aguilo.miguel@gmail.com>
2020-12-15 12:25:18 -0700

    MomentumSurfaceForces

--- unit_tests/ComputationalFluidDynamicsTests.cpp
+++ unit_tests/ComputationalFluidDynamicsTests.cpp
@@ -3917,15 +3917,15 @@ private:
     using ConfigT    = typename EvaluationT::ConfigScalarType;
     using PrevVelT   = typename EvaluationT::PreviousMomentumScalarType;
 
-    const std::string mEntityName; /*!< side set name */
+    const std::string mEntitySetName; /*!< side set name */
     const Plato::SpatialDomain& mSpatialDomain; /*!< Plato spatial model */
     Plato::LinearTetCubRuleDegreeOne<mNumSpatialDims> mCubatureRule;  /*!< integration rule */
 
 public:
     MomentumSurfaceForces
     (const Plato::SpatialDomain & aSpatialDomain,
-     const std::string & aEntityName) :
-         mEntityName(aEntityName),
+     const std::string & aEntitySetName) :
+         mEntitySetName(aEntitySetName),
          mSpatialDomain(aSpatialDomain)
     {
     }
@@ -3955,7 +3955,7 @@ public:
         Plato::CreateFaceLocalNode2ElemLocalNodeIndexMap<mNumSpatialDims> tCreateFaceLocalNode2ElemLocalNodeIndexMap;
 
         // get sideset faces
-        auto tFaceLocalOrdinals = Plato::side_set_face_ordinals(mSpatialDomain.MeshSets, mEntityName);
+        auto tFaceLocalOrdinals = Plato::side_set_face_ordinals(mSpatialDomain.MeshSets, mEntitySetName);
         auto tNumFaces = tFaceLocalOrdinals.size();
         Plato::ScalarArray3DT<ConfigT> tJacobians("jacobian", tNumFaces, mNumSpatialDimsOnFace, mNumSpatialDims);
 
@@ -6584,6 +6584,81 @@ private:
 
 namespace ComputationalFluidDynamicsTests
 {
+
+// todo unit test
+TEUCHOS_UNIT_TEST(PlatoAnalyzeUnitTests, MomentumSurfaceForces)
+{
+    // set physics and evaluation type
+    constexpr Plato::OrdinalType tNumSpaceDims = 2;
+    using PhysicsT = Plato::IncompressibleFluids<tNumSpaceDims>::MassPhysicsT;
+    using EvaluationT = Plato::Fluids::Evaluation<PhysicsT::SimplexT>::Residual;
+
+    // build mesh, spatial domain, and spatial model
+    auto tMesh = PlatoUtestHelpers::build_2d_box_mesh(1,1,1,1);
+    auto tMeshSets = PlatoUtestHelpers::get_box_mesh_sets(tMesh.operator*());
+    auto tSpatialDomainName = std::string("my box");
+    Plato::SpatialDomain tDomain(tMesh.operator*(), tMeshSets, tSpatialDomainName);
+    auto tElementBlockName = std::string("body");
+    tDomain.cellOrdinals(tElementBlockName);
+
+    // set workset
+    Plato::WorkSets tWorkSets;
+    auto tNumCells = tMesh->nelems();
+    auto tNumNodesPerCell = tNumSpaceDims + 1;
+    using ConfigT = EvaluationT::ConfigScalarType;
+    Plato::NodeCoordinate<tNumSpaceDims> tNodeCoordinate( (&tMesh.operator*()) );
+    auto tConfig = std::make_shared< Plato::MetaData< Plato::ScalarArray3DT<ConfigT> > >
+        ( Plato::ScalarArray3DT<ConfigT>("configuration", tNumCells, tNumNodesPerCell, tNumSpaceDims) );
+    Plato::workset_config_scalar<tNumSpaceDims, tNumNodesPerCell>(tMesh->nelems(), tNodeCoordinate, tConfig->mData);
+    tWorkSets.set("configuration", tConfig);
+
+    using PrevVelT = EvaluationT::PreviousMomentumScalarType;
+    auto tPrevVel = std::make_shared< Plato::MetaData< Plato::ScalarMultiVectorT<PrevVelT> > >
+        ( Plato::ScalarMultiVectorT<PrevVelT>("previous velocity", tNumCells, PhysicsT::mNumMomentumDofsPerCell) );
+    auto tHostVelocity = Kokkos::create_mirror(tPrevVel->mData);
+    tHostVelocity(0, 0) = 1; tHostVelocity(1, 0) = 11;
+    tHostVelocity(0, 1) = 2; tHostVelocity(1, 1) = 12;
+    tHostVelocity(0, 2) = 3; tHostVelocity(1, 2) = 13;
+    tHostVelocity(0, 3) = 4; tHostVelocity(1, 3) = 14;
+    tHostVelocity(0, 4) = 5; tHostVelocity(1, 4) = 15;
+    tHostVelocity(0, 5) = 6; tHostVelocity(1, 5) = 16;
+    Kokkos::deep_copy(tPrevVel->mData, tHostVelocity);
+    tWorkSets.set("previous velocity", tPrevVel);
+
+    auto tPrescribedVel = std::make_shared< Plato::MetaData< Plato::ScalarMultiVector > >
+        ( Plato::ScalarMultiVector("prescribed velocity", tNumCells, PhysicsT::mNumMomentumDofsPerCell) );
+    auto tHostPrescribedVel = Kokkos::create_mirror(tPrescribedVel->mData);
+    tHostPrescribedVel(0, 0) = 0.1; tHostPrescribedVel(1, 0) = 0.7;
+    tHostPrescribedVel(0, 1) = 0.2; tHostPrescribedVel(1, 1) = 0.8;
+    tHostPrescribedVel(0, 2) = 0.3; tHostPrescribedVel(1, 2) = 0.9;
+    tHostPrescribedVel(0, 3) = 0.4; tHostPrescribedVel(1, 3) = 1.0;
+    tHostPrescribedVel(0, 4) = 0.5; tHostPrescribedVel(1, 4) = 1.1;
+    tHostPrescribedVel(0, 5) = 0.6; tHostPrescribedVel(1, 5) = 1.2;
+    Kokkos::deep_copy(tPrescribedVel->mData, tHostPrescribedVel);
+    tWorkSets.set("prescribed velocity", tPrescribedVel);
+
+    // build momentum surface forces interface and call funciton
+    auto tSideSetName = std::string("x-");
+    Plato::Fluids::MomentumSurfaceForces<PhysicsT, EvaluationT> tMomentumBoundaryForces(tDomain, tSideSetName);
+    Plato::ScalarMultiVectorT<EvaluationT::ResultScalarType> tResult("result", tNumCells, PhysicsT::mNumMassDofsPerCell);
+    tMomentumBoundaryForces(tWorkSets, tResult);
+
+    // test values
+    /*
+    auto tTol = 1e-4;
+    auto tHostResult = Kokkos::create_mirror(tResult);
+    Kokkos::deep_copy(tHostResult, tResult);
+    std::vector<std::vector<Plato::Scalar>> tGold = {{0.0,0.0,0.0,0.0},{0.0,0.0,0.0,0.0}};
+    for (Plato::OrdinalType tCell = 0; tCell < tNumCells; tCell++)
+    {
+        for (Plato::OrdinalType tDof = 0; tDof < PhysicsT::mNumMomentumDofsPerCell; tDof++)
+        {
+            TEST_FLOATING_EQUALITY(tGold[tCell][tDof], tHostResult(tCell, tDof), tTol);
+        }
+    }
+    */
+    Plato::print_array_2D(tResult, "results");
+}
 
 // todo unit test
 TEUCHOS_UNIT_TEST(PlatoAnalyzeUnitTests, TemperatureIncrementResidual)
