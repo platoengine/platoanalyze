e7e79587683545ec87ae0dd35ccbed442673a92e
maguilo11 <aguilo.miguel@gmail.com>
2021-01-12 14:32:05 -0700

    calculate pressure residual unit test

--- unit_tests/ComputationalFluidDynamicsTests.cpp
+++ unit_tests/ComputationalFluidDynamicsTests.cpp
@@ -6037,7 +6037,6 @@ calculate_pressure_residual
         auto tDeltaPressOverTimeStep = ( aCurrentPressure(aNodeOrdinal) - aPreviousPressure(aNodeOrdinal) ) / aTimeStep(aNodeOrdinal);
         auto tOneOverBetaSquared = static_cast<Plato::Scalar>(1) / ( aArtificialCompress(aNodeOrdinal) * aArtificialCompress(aNodeOrdinal) );
         tResidual(aNodeOrdinal) = tOneOverBetaSquared * tDeltaPressOverTimeStep;
-        tResidual(aNodeOrdinal) *= tResidual(aNodeOrdinal);
     }, "calculate stopping criterion");
 
     return tResidual;
@@ -6892,6 +6891,60 @@ private:
 
 namespace ComputationalFluidDynamicsTests
 {
+
+TEUCHOS_UNIT_TEST(PlatoAnalyzeUnitTests, CalculatePressureResidual)
+{
+    // set time step
+    constexpr auto tNumNodes = 4;
+    Plato::ScalarVector tTimeStep("time step", tNumNodes);
+    auto tHostTimeStep = Kokkos::create_mirror(tTimeStep);
+    tHostTimeStep(0) = 0.1;
+    tHostTimeStep(1) = 0.2;
+    tHostTimeStep(2) = 0.3;
+    tHostTimeStep(3) = 0.4;
+    Kokkos::deep_copy(tTimeStep, tHostTimeStep);
+
+    // set element characteristic size
+    Plato::ScalarVector tCurPressure("current pressure", tNumNodes);
+    auto tHostCurPressure = Kokkos::create_mirror(tCurPressure);
+    tHostCurPressure(0) = 1;
+    tHostCurPressure(1) = 2;
+    tHostCurPressure(2) = 3;
+    tHostCurPressure(3) = 4;
+    Kokkos::deep_copy(tCurPressure, tHostCurPressure);
+
+    // set convective velocity
+    Plato::ScalarVector tPrevPressure("previous pressure", tNumNodes);
+    auto tHostPrevPressure = Kokkos::create_mirror(tPrevPressure);
+    tHostPrevPressure(0) = 0.5;
+    tHostPrevPressure(1) = 0.6;
+    tHostPrevPressure(2) = 0.7;
+    tHostPrevPressure(3) = 0.8;
+    Kokkos::deep_copy(tPrevPressure, tHostPrevPressure);
+
+    // set artificial compressibility
+    Plato::ScalarVector tArtificialCompress("artificial compressibility", tNumNodes);
+    auto tHostArtificialCompress = Kokkos::create_mirror(tArtificialCompress);
+    tHostArtificialCompress(0) = 0.1;
+    tHostArtificialCompress(1) = 0.2;
+    tHostArtificialCompress(2) = 0.3;
+    tHostArtificialCompress(3) = 0.4;
+    Kokkos::deep_copy(tArtificialCompress, tHostArtificialCompress);
+
+    // call function
+    auto tResidual = Plato::cbs::calculate_pressure_residual(tTimeStep, tCurPressure, tPrevPressure, tArtificialCompress);
+
+    // test results
+    auto tTol = 1e-4;
+    auto tHostResidual = Kokkos::create_mirror(tResidual);
+    Kokkos::deep_copy(tHostResidual, tResidual);
+    std::vector<Plato::Scalar> tGold = {500,175,85.185185185,50};
+    for (decltype(tNumNodes) tNode = 0; tNode < tNumNodes; tNode++)
+    {
+        TEST_FLOATING_EQUALITY(tGold[tNode], tHostResidual(tNode), tTol);
+    }
+    //Plato::print(tResidual, "residual");
+}
 
 TEUCHOS_UNIT_TEST(PlatoAnalyzeUnitTests, CalculateStableTimeStep)
 {
