985a4a380152fceca92374438ebcf53d0e1b0c77
maguilo11 <aguilo.miguel@gmail.com>
2021-04-08 12:23:14 -0600

    explicit instantiation

--- src/hyperbolic/VelocityPredictorResidual.hpp
+++ src/hyperbolic/VelocityPredictorResidual.hpp
@@ -13,12 +13,14 @@
 #include "FluidsUtils.hpp"
 #include "UtilsTeuchos.hpp"
 #include "SpatialModel.hpp"
+#include "ExpInstMacros.hpp"
 #include "InterpolateFromNodal.hpp"
 #include "LinearTetCubRuleDegreeOne.hpp"
 
 #include "hyperbolic/BrinkmanForces.hpp"
 #include "hyperbolic/ThermalBuoyancy.hpp"
 #include "hyperbolic/SimplexFluidsFadTypes.hpp"
+#include "hyperbolic/AbstractVectorFunction.hpp"
 #include "hyperbolic/MomentumConservationUtils.hpp"
 
 namespace Plato
@@ -393,3 +395,15 @@ private:
 
 }
 // namespace Plato
+
+#ifdef PLATOANALYZE_1D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::VelocityPredictorResidual, Plato::MomentumConservation, Plato::SimplexFluids, 1, 1)
+#endif
+
+#ifdef PLATOANALYZE_2D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::VelocityPredictorResidual, Plato::MomentumConservation, Plato::SimplexFluids, 2, 1)
+#endif
+
+#ifdef PLATOANALYZE_3D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::VelocityPredictorResidual, Plato::MomentumConservation, Plato::SimplexFluids, 3, 1)
+#endif
