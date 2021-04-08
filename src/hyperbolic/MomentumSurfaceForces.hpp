751f985b65a95ab7bc5806a5384ded07a9f6fc49
maguilo11 <aguilo.miguel@gmail.com>
2021-04-08 16:51:18 -0600

    explicit instantiations - momentum surface forces

--- src/hyperbolic/MomentumSurfaceForces.hpp
+++ src/hyperbolic/MomentumSurfaceForces.hpp
@@ -11,9 +11,13 @@
 #include "NaturalBCs.hpp"
 #include "UtilsOmegaH.hpp"
 #include "SpatialModel.hpp"
+#include "ExpInstMacros.hpp"
 #include "InterpolateFromNodal.hpp"
 #include "AbstractVolumeIntegrand.hpp"
 #include "LinearTetCubRuleDegreeOne.hpp"
+
+#include "hyperbolic/SimplexFluids.hpp"
+#include "hyperbolic/SimplexFluidsFadTypes.hpp"
 
 namespace Plato
 {
@@ -206,3 +210,17 @@ public:
 
 }
 // namespace Plato
+
+#include "hyperbolic/IncompressibleFluids.hpp"
+
+#ifdef PLATOANALYZE_1D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::MomentumSurfaceForces, Plato::MassConservation, Plato::SimplexFluids, 1, 1)
+#endif
+
+#ifdef PLATOANALYZE_2D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::MomentumSurfaceForces, Plato::MassConservation, Plato::SimplexFluids, 2, 1)
+#endif
+
+#ifdef PLATOANALYZE_3D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::MomentumSurfaceForces, Plato::MassConservation, Plato::SimplexFluids, 3, 1)
+#endif
