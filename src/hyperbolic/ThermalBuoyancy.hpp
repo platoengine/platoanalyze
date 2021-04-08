d6cba940005a67fb189d9606b0560c9c2ff2694f
maguilo11 <aguilo.miguel@gmail.com>
2021-04-08 16:44:43 -0600

    explicit instantiations - thermal buoyancy

--- src/hyperbolic/ThermalBuoyancy.hpp
+++ src/hyperbolic/ThermalBuoyancy.hpp
@@ -12,9 +12,11 @@
 #include "WorkSets.hpp"
 #include "FluidsUtils.hpp"
 #include "SpatialModel.hpp"
+#include "ExpInstMacros.hpp"
 #include "InterpolateFromNodal.hpp"
 #include "LinearTetCubRuleDegreeOne.hpp"
 
+#include "hyperbolic/SimplexFluids.hpp"
 #include "hyperbolic/SimplexFluidsFadTypes.hpp"
 #include "hyperbolic/MomentumConservationUtils.hpp"
 
@@ -181,3 +183,17 @@ private:
 
 }
 // namespace Plato
+
+#include "hyperbolic/IncompressibleFluids.hpp"
+
+#ifdef PLATOANALYZE_1D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::ThermalBuoyancy, Plato::MomentumConservation, Plato::SimplexFluids, 1, 1)
+#endif
+
+#ifdef PLATOANALYZE_2D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::ThermalBuoyancy, Plato::MomentumConservation, Plato::SimplexFluids, 2, 1)
+#endif
+
+#ifdef PLATOANALYZE_3D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::ThermalBuoyancy, Plato::MomentumConservation, Plato::SimplexFluids, 3, 1)
+#endif
