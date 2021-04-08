430c763a4f2b18ca12fa8c83ef3e71e13033eebf
maguilo11 <aguilo.miguel@gmail.com>
2021-04-08 17:08:14 -0600

    explicit instantiation - InternalThermalForces

--- src/hyperbolic/InternalThermalForces.hpp
+++ src/hyperbolic/InternalThermalForces.hpp
@@ -12,11 +12,13 @@
 #include "MetaData.hpp"
 #include "SpatialModel.hpp"
 #include "UtilsTeuchos.hpp"
+#include "ExpInstMacros.hpp"
 #include "InterpolateFromNodal.hpp"
 #include "AbstractVolumeIntegrand.hpp"
 #include "LinearTetCubRuleDegreeOne.hpp"
 
 #include "hyperbolic/FluidsUtils.hpp"
+#include "hyperbolic/SimplexFluids.hpp"
 #include "hyperbolic/SimplexFluidsFadTypes.hpp"
 #include "hyperbolic/EnergyConservationUtils.hpp"
 
@@ -599,3 +601,20 @@ private:
 
 }
 // namespace Plato
+
+#include "hyperbolic/IncompressibleFluids.hpp"
+
+#ifdef PLATOANALYZE_1D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::InternalThermalForces, Plato::EnergyConservation, Plato::SimplexFluids, 1, 1)
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::SIMP::InternalThermalForces, Plato::EnergyConservation, Plato::SimplexFluids, 1, 1)
+#endif
+
+#ifdef PLATOANALYZE_2D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::InternalThermalForces, Plato::EnergyConservation, Plato::SimplexFluids, 2, 1)
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::SIMP::InternalThermalForces, Plato::EnergyConservation, Plato::SimplexFluids, 2, 1)
+#endif
+
+#ifdef PLATOANALYZE_3D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::InternalThermalForces, Plato::EnergyConservation, Plato::SimplexFluids, 3, 1)
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::SIMP::InternalThermalForces, Plato::EnergyConservation, Plato::SimplexFluids, 3, 1)
+#endif
