f1f7fe2e5ca334534e402d542d567722622d1939
maguilo11 <aguilo.miguel@gmail.com>
2021-04-08 17:47:23 -0600

    explicit instantiation - AverageSurfaceTemperature

--- src/hyperbolic/AverageSurfaceTemperature.hpp
+++ src/hyperbolic/AverageSurfaceTemperature.hpp
@@ -9,9 +9,12 @@
 #include "UtilsOmegaH.hpp"
 #include "SpatialModel.hpp"
 #include "UtilsTeuchos.hpp"
+#include "ExpInstMacros.hpp"
 #include "SurfaceIntegralUtilities.hpp"
 #include "LinearTetCubRuleDegreeOne.hpp"
 
+#include "hyperbolic/SimplexFluids.hpp"
+#include "hyperbolic/SimplexFluidsFadTypes.hpp"
 #include "hyperbolic/AbstractScalarFunction.hpp"
 
 namespace Plato
@@ -190,3 +193,17 @@ public:
 
 }
 // namespace Plato
+
+#include "hyperbolic/IncompressibleFluids.hpp"
+
+#ifdef PLATOANALYZE_1D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::AverageSurfaceTemperature, Plato::IncompressibleFluids, Plato::SimplexFluids, 1, 1)
+#endif
+
+#ifdef PLATOANALYZE_2D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::AverageSurfaceTemperature, Plato::IncompressibleFluids, Plato::SimplexFluids, 2, 1)
+#endif
+
+#ifdef PLATOANALYZE_3D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::AverageSurfaceTemperature, Plato::IncompressibleFluids, Plato::SimplexFluids, 3, 1)
+#endif
