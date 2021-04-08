1d6f5eb16f11b2d6ee6cfb69dd404c54b71f8586
maguilo11 <aguilo.miguel@gmail.com>
2021-04-08 16:13:06 -0600

    explicit instantiation

--- src/hyperbolic/VelocityCorrectorResidual.hpp
+++ src/hyperbolic/VelocityCorrectorResidual.hpp
@@ -16,6 +16,7 @@
 #include "InterpolateFromNodal.hpp"
 #include "LinearTetCubRuleDegreeOne.hpp"
 
+#include "hyperbolic/SimplexFluids.hpp"
 #include "hyperbolic/BrinkmanForces.hpp"
 #include "hyperbolic/ThermalBuoyancy.hpp"
 #include "hyperbolic/SimplexFluidsFadTypes.hpp"
@@ -220,3 +221,18 @@ private:
 
 }
 // namespace Plato
+
+#include "hyperbolic/IncompressibleFluids.hpp"
+
+#ifdef PLATOANALYZE_1D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::VelocityCorrectorResidual, Plato::MomentumConservation, Plato::SimplexFluids, 1, 1)
+#endif
+
+#ifdef PLATOANALYZE_2D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::VelocityCorrectorResidual, Plato::MomentumConservation, Plato::SimplexFluids, 2, 1)
+#endif
+
+#ifdef PLATOANALYZE_3D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::VelocityCorrectorResidual, Plato::MomentumConservation, Plato::SimplexFluids, 3, 1)
+#endif
+
