a7b2530a9688ad0acf2d89d1f166a05038d43889
maguilo11 <aguilo.miguel@gmail.com>
2021-04-08 17:52:30 -0600

    explicit instantiation - AverageSurfacePressure

--- src/hyperbolic/AverageSurfacePressure.hpp
+++ src/hyperbolic/AverageSurfacePressure.hpp
@@ -9,9 +9,12 @@
 #include "UtilsOmegaH.hpp"
 #include "SpatialModel.hpp"
 #include "UtilsTeuchos.hpp"
+#include "ExpInstMacros.hpp"
 #include "SurfaceIntegralUtilities.hpp"
 #include "LinearTetCubRuleDegreeOne.hpp"
 
+#include "hyperbolic/SimplexFluids.hpp"
+#include "hyperbolic/SimplexFluidsFadTypes.hpp"
 #include "hyperbolic/AbstractScalarFunction.hpp"
 
 namespace Plato
@@ -183,3 +186,17 @@ public:
 
 }
 // namespace Plato
+
+#include "hyperbolic/IncompressibleFluids.hpp"
+
+#ifdef PLATOANALYZE_1D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::AverageSurfacePressure, Plato::IncompressibleFluids, Plato::SimplexFluids, 1, 1)
+#endif
+
+#ifdef PLATOANALYZE_2D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::AverageSurfacePressure, Plato::IncompressibleFluids, Plato::SimplexFluids, 2, 1)
+#endif
+
+#ifdef PLATOANALYZE_3D
+PLATO_EXPL_DEC_FLUIDS(Plato::Fluids::AverageSurfacePressure, Plato::IncompressibleFluids, Plato::SimplexFluids, 3, 1)
+#endif
